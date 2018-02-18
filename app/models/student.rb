dd60ad7408aaaa5b913650ebb562abcdbeb26c22
Min Kim <mk4555@nyu.edu>
2018-02-18 18:58:32 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,7 +15,6 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
 
   def self.search(student)
-    @student = []
     if student.empty?
       @student = self.all
     else
